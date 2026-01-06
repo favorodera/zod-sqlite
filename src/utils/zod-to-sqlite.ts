4b33e1918abf34c840c5da61dc27ffa67e3cac36
Favour  Emeka <favorodera@gmail.com>
2026-01-06 12:50:09 +0100

    feat(zod-sqlite): add zodToSQLite function to map Zod schemas to SQLite types

---
+++ src/utils/zod-to-sqlite.ts
@@ -0,0 +1,27 @@
+import * as zod from 'zod/v4/core'
+import { mapZodTypeToSQLite } from './map-zod-type-to-sqlite'
+
+export function zodToSQLite(schema: zod.$ZodType) {
+  let nullable = false
+  let defaultValue = undefined
+  let currentSchema = schema as zod.$ZodTypes
+  // Unwrap wrapped schemas and collect metadata
+  while (true) {
+    const definition = currentSchema._zod.def
+    const zodType = definition.type
+    if (zodType === 'nullable' || zodType === 'optional') {
+      nullable = true
+      currentSchema = definition.innerType as zod.$ZodTypes
+    } else if (zodType === 'default') {
+      defaultValue = typeof definition.defaultValue === 'function'
+        ? definition.defaultValue()
+        : definition.defaultValue
+      currentSchema = definition.innerType as zod.$ZodTypes
+    } else {
+      break
+    }
+  }
+  const mainType = currentSchema._zod.def.type
+  const SQLiteType = mapZodTypeToSQLite(mainType, currentSchema)
+  return { SQLiteType, nullable, defaultValue, schema }
+}
