631c7a070a92c98bcf1d316f4b59d593aceda6ec
Favour  Emeka <favorodera@gmail.com>
2026-01-06 15:58:22 +0100

    refactor(primary-key): accept readonly string array for primary key constraint

--- src/core/primary-key-constraint.ts
+++ src/core/primary-key-constraint.ts
@@ -1,4 +1,4 @@
-export function buildPrimaryKeyConstraint(primaryKeys: string[]): string {
+export function buildPrimaryKeyConstraint(primaryKeys: string[] | readonly string[]): string {
   if (primaryKeys.length === 0) return ''
   return `PRIMARY KEY (${primaryKeys.join(', ')})`
 }
